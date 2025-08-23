38515cbcd6bf575af35b517745d041ad6f0a23e9
SHARIF IQBAL JAMIL <msijmpshaan369@gmail.com>
2025-08-23 21:11:58 +0530

    hero and about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,43 +1,36 @@
+import aboutImg from '/images/about.jpeg';
+
 const About = () => {
   return (
     <section className='section' id='about'>
-    {/* <!-- title  --> */}
-    <div className='section-title'>
-      <h2>
-        about <span>us</span>
-      </h2>
-    </div>
-    {/* <!-- end of title  --> */}
-    {/* <!-- about-center --> */}
-    <div className='section-center about-center'>
-      {/* <!-- about img wrapper--> */}
-      <div className='about-img'>
-        <img
-          src='./images/about.jpeg'
-          className='about-photo'
-          alt='awesome beach'
-        />
+      <div className='section-title'>
+        <h2>
+          about <span>us</span>
+        </h2>
       </div>
-      {/* <!-- about info --> */}
-      <article className='about-info'>
-        <h3>explore the difference</h3>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          Aspernatur quisquam harum nam cumque temporibus explicabo dolorum
-          sapiente odio unde dolor?
-        </p>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          Aspernatur quisquam harum nam cumque temporibus explicabo dolorum
-          sapiente odio unde dolor?
-        </p>
-        <a href='#' className='btn'>
-          read more
-        </a>
-      </article>
-    </div>
-  </section>
-  )
-}
+      <div className='section-center about-center'>
+        <div className='about-img'>
+          <img src={aboutImg} className='about-photo' alt='awesome beach' />
+        </div>
+        <article className='about-info'>
+          <h3>explore the difference</h3>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
+            quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
+            unde dolor?
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
+            quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
+            unde dolor?
+          </p>
+          <a href='#' className='btn'>
+            read more
+          </a>
+        </article>
+      </div>
+    </section>
+  );
+};
 
-export default About
+export default About;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,6 @@
 const Hero = () => {
   return (
-    <div
-      className='hero'
-      style={{
-        position: 'relative',
-        backgroundImage: "url('./images/main.jpeg')",
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        overflow: 'hidden',
-      }}
-    >
-      <div
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-          background: 'rgba(0, 0, 0, 0.5)',
-          zIndex: 1,
-        }}
-      ></div>
+    <div className='hero'>
       <div className='hero-banner' style={{ position: 'relative', zIndex: 2 }}>
         <h1>continue exploring</h1>
         <p>
