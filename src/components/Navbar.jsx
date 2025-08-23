ce1c3e5e6c7801cdecbfa7c73aba5848c6c72362
SHARIF IQBAL JAMIL <msijmpshaan369@gmail.com>
2025-08-23 17:12:01 +0530

    social links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { pageLinks } from '../data';
+import { pageLinks, socialLinks } from '../data';
 import logo from '/images/logo.svg';
 
 const Navbar = () => {
@@ -21,33 +21,13 @@ const Navbar = () => {
           ))}
         </ul>
         <ul className='nav-icons'>
-          <li>
-            <a
-              href='https://www.twitter.com'
-              target='_blank'
-              className='nav-icon'
-            >
-              <i className='fab fa-facebook'></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href='https://www.twitter.com'
-              target='_blank'
-              className='nav-icon'
-            >
-              <i className='fab fa-twitter'></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href='https://www.twitter.com'
-              target='_blank'
-              className='nav-icon'
-            >
-              <i className='fab fa-squarespace'></i>
-            </a>
-          </li>
+          {socialLinks.map((link) => (
+            <li key={link.id}>
+              <a href={link.href} target='_blank' className='nav-icon'>
+                <i className={link.icon}></i>
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
     </nav>
--- src/data.js
+++ src/data.js
@@ -5,3 +5,21 @@ export const pageLinks = [
   { id: 4, href: '#featured', text: 'Featured' },
   { id: 5, href: '#gallery', text: 'Gallery' },
 ];
+
+export const socialLinks = [
+  {
+    id: 1,
+    href: 'https://www.facebook.com',
+    icon: 'fab fa-facebook',
+  },
+  {
+    id: 2,
+    href: 'https://www.twitter.com',
+    icon: 'fab fa-twitter',
+  },
+  {
+    id: 3,
+    href: 'https://www.squarespace.com',
+    icon: 'fab fa-squarespace',
+  },
+];
