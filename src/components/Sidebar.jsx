827e77394a9908b2657aff6a5f9a80eeb1580e6c
SHARIF IQBAL JAMIL <msijmpshaan369@gmail.com>
2025-10-09 10:27:38 +0530

    feat(cart): add interactive shopping cart to sidebar
    Introduce a detailed cart interface in the Sidebar component featuring item listings with visuals, pricing, quantity modifiers, deletion options, order total, and a checkout prompt. Enhance sidebar usability with tailored scrollbar design in index.css.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,305 @@
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div id='sidebar' className='bg-white fixed top-0 right-0 z-50 h-screen w-full md:w-3/5 lg:w-2/5 shadow-2xl p-8 flex flex-col'>
+      <div className='flex items-center justify-between pb-6 px-2'>
+        <div className='font-semibold'>ORDER</div>
+        <i className='ri-close-large-line'></i>
+      </div>
 
-export default Sidebar
+      <div className='max-h-[72vh] overflow-auto pt-8 border-t border-zinc-300 flex flex-col gap-16'>
+        <div className='flex gap-4 justify-between p-2'>
+          <img
+            src='https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png'
+            alt=''
+            className='w-20'
+          />
+          <div className='mr-auto flex flex-col justify-between'>
+            <div className='line-clamp-1 font-semibold'>
+              Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+            </div>
+            <div>Rs. 88759.11</div>
+            <div className='flex border border-zinc-300 w-fit py-0.5'>
+              <button type='button' className='w-8'>
+                <i className='ri-subtract-fill text-red-600'></i>
+              </button>
+              <div className='text-center'>0</div>
+              <button type='button' className='w-8'>
+                <i className='ri-add-fill text-green-600'></i>
+              </button>
+            </div>
+          </div>
+          <div className='flex flex-col min-w-fit'>
+            <i className='ri-close-large-line ml-auto bg-red-600 text-white p-1.5'></i>
+            <div className='mt-auto ml-auto font-semibold'>Rs. 88759.11</div>
+          </div>
+        </div>        <div className='flex gap-4 justify-between p-2'>
+          <img
+            src='https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png'
+            alt=''
+            className='w-20'
+          />
+          <div className='mr-auto flex flex-col justify-between'>
+            <div className='line-clamp-1 font-semibold'>
+              Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+            </div>
+            <div>Rs. 88759.11</div>
+            <div className='flex border border-zinc-300 w-fit py-0.5'>
+              <button type='button' className='w-8'>
+                <i className='ri-subtract-fill text-red-600'></i>
+              </button>
+              <div className='text-center'>0</div>
+              <button type='button' className='w-8'>
+                <i className='ri-add-fill text-green-600'></i>
+              </button>
+            </div>
+          </div>
+          <div className='flex flex-col min-w-fit'>
+            <i className='ri-close-large-line ml-auto bg-red-600 text-white p-1.5'></i>
+            <div className='mt-auto ml-auto font-semibold'>Rs. 88759.11</div>
+          </div>
+        </div>        <div className='flex gap-4 justify-between p-2'>
+          <img
+            src='https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png'
+            alt=''
+            className='w-20'
+          />
+          <div className='mr-auto flex flex-col justify-between'>
+            <div className='line-clamp-1 font-semibold'>
+              Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+            </div>
+            <div>Rs. 88759.11</div>
+            <div className='flex border border-zinc-300 w-fit py-0.5'>
+              <button type='button' className='w-8'>
+                <i className='ri-subtract-fill text-red-600'></i>
+              </button>
+              <div className='text-center'>0</div>
+              <button type='button' className='w-8'>
+                <i className='ri-add-fill text-green-600'></i>
+              </button>
+            </div>
+          </div>
+          <div className='flex flex-col min-w-fit'>
+            <i className='ri-close-large-line ml-auto bg-red-600 text-white p-1.5'></i>
+            <div className='mt-auto ml-auto font-semibold'>Rs. 88759.11</div>
+          </div>
+        </div>        <div className='flex gap-4 justify-between p-2'>
+          <img
+            src='https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png'
+            alt=''
+            className='w-20'
+          />
+          <div className='mr-auto flex flex-col justify-between'>
+            <div className='line-clamp-1 font-semibold'>
+              Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+            </div>
+            <div>Rs. 88759.11</div>
+            <div className='flex border border-zinc-300 w-fit py-0.5'>
+              <button type='button' className='w-8'>
+                <i className='ri-subtract-fill text-red-600'></i>
+              </button>
+              <div className='text-center'>0</div>
+              <button type='button' className='w-8'>
+                <i className='ri-add-fill text-green-600'></i>
+              </button>
+            </div>
+          </div>
+          <div className='flex flex-col min-w-fit'>
+            <i className='ri-close-large-line ml-auto bg-red-600 text-white p-1.5'></i>
+            <div className='mt-auto ml-auto font-semibold'>Rs. 88759.11</div>
+          </div>
+        </div>        <div className='flex gap-4 justify-between p-2'>
+          <img
+            src='https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png'
+            alt=''
+            className='w-20'
+          />
+          <div className='mr-auto flex flex-col justify-between'>
+            <div className='line-clamp-1 font-semibold'>
+              Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+            </div>
+            <div>Rs. 88759.11</div>
+            <div className='flex border border-zinc-300 w-fit py-0.5'>
+              <button type='button' className='w-8'>
+                <i className='ri-subtract-fill text-red-600'></i>
+              </button>
+              <div className='text-center'>0</div>
+              <button type='button' className='w-8'>
+                <i className='ri-add-fill text-green-600'></i>
+              </button>
+            </div>
+          </div>
+          <div className='flex flex-col min-w-fit'>
+            <i className='ri-close-large-line ml-auto bg-red-600 text-white p-1.5'></i>
+            <div className='mt-auto ml-auto font-semibold'>Rs. 88759.11</div>
+          </div>
+        </div>        <div className='flex gap-4 justify-between p-2'>
+          <img
+            src='https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png'
+            alt=''
+            className='w-20'
+          />
+          <div className='mr-auto flex flex-col justify-between'>
+            <div className='line-clamp-1 font-semibold'>
+              Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+            </div>
+            <div>Rs. 88759.11</div>
+            <div className='flex border border-zinc-300 w-fit py-0.5'>
+              <button type='button' className='w-8'>
+                <i className='ri-subtract-fill text-red-600'></i>
+              </button>
+              <div className='text-center'>0</div>
+              <button type='button' className='w-8'>
+                <i className='ri-add-fill text-green-600'></i>
+              </button>
+            </div>
+          </div>
+          <div className='flex flex-col min-w-fit'>
+            <i className='ri-close-large-line ml-auto bg-red-600 text-white p-1.5'></i>
+            <div className='mt-auto ml-auto font-semibold'>Rs. 88759.11</div>
+          </div>
+        </div>        <div className='flex gap-4 justify-between p-2'>
+          <img
+            src='https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png'
+            alt=''
+            className='w-20'
+          />
+          <div className='mr-auto flex flex-col justify-between'>
+            <div className='line-clamp-1 font-semibold'>
+              Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+            </div>
+            <div>Rs. 88759.11</div>
+            <div className='flex border border-zinc-300 w-fit py-0.5'>
+              <button type='button' className='w-8'>
+                <i className='ri-subtract-fill text-red-600'></i>
+              </button>
+              <div className='text-center'>0</div>
+              <button type='button' className='w-8'>
+                <i className='ri-add-fill text-green-600'></i>
+              </button>
+            </div>
+          </div>
+          <div className='flex flex-col min-w-fit'>
+            <i className='ri-close-large-line ml-auto bg-red-600 text-white p-1.5'></i>
+            <div className='mt-auto ml-auto font-semibold'>Rs. 88759.11</div>
+          </div>
+        </div>        <div className='flex gap-4 justify-between p-2'>
+          <img
+            src='https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png'
+            alt=''
+            className='w-20'
+          />
+          <div className='mr-auto flex flex-col justify-between'>
+            <div className='line-clamp-1 font-semibold'>
+              Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+            </div>
+            <div>Rs. 88759.11</div>
+            <div className='flex border border-zinc-300 w-fit py-0.5'>
+              <button type='button' className='w-8'>
+                <i className='ri-subtract-fill text-red-600'></i>
+              </button>
+              <div className='text-center'>0</div>
+              <button type='button' className='w-8'>
+                <i className='ri-add-fill text-green-600'></i>
+              </button>
+            </div>
+          </div>
+          <div className='flex flex-col min-w-fit'>
+            <i className='ri-close-large-line ml-auto bg-red-600 text-white p-1.5'></i>
+            <div className='mt-auto ml-auto font-semibold'>Rs. 88759.11</div>
+          </div>
+        </div>        <div className='flex gap-4 justify-between p-2'>
+          <img
+            src='https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png'
+            alt=''
+            className='w-20'
+          />
+          <div className='mr-auto flex flex-col justify-between'>
+            <div className='line-clamp-1 font-semibold'>
+              Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+            </div>
+            <div>Rs. 88759.11</div>
+            <div className='flex border border-zinc-300 w-fit py-0.5'>
+              <button type='button' className='w-8'>
+                <i className='ri-subtract-fill text-red-600'></i>
+              </button>
+              <div className='text-center'>0</div>
+              <button type='button' className='w-8'>
+                <i className='ri-add-fill text-green-600'></i>
+              </button>
+            </div>
+          </div>
+          <div className='flex flex-col min-w-fit'>
+            <i className='ri-close-large-line ml-auto bg-red-600 text-white p-1.5'></i>
+            <div className='mt-auto ml-auto font-semibold'>Rs. 88759.11</div>
+          </div>
+        </div>        <div className='flex gap-4 justify-between p-2'>
+          <img
+            src='https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png'
+            alt=''
+            className='w-20'
+          />
+          <div className='mr-auto flex flex-col justify-between'>
+            <div className='line-clamp-1 font-semibold'>
+              Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+            </div>
+            <div>Rs. 88759.11</div>
+            <div className='flex border border-zinc-300 w-fit py-0.5'>
+              <button type='button' className='w-8'>
+                <i className='ri-subtract-fill text-red-600'></i>
+              </button>
+              <div className='text-center'>0</div>
+              <button type='button' className='w-8'>
+                <i className='ri-add-fill text-green-600'></i>
+              </button>
+            </div>
+          </div>
+          <div className='flex flex-col min-w-fit'>
+            <i className='ri-close-large-line ml-auto bg-red-600 text-white p-1.5'></i>
+            <div className='mt-auto ml-auto font-semibold'>Rs. 88759.11</div>
+          </div>
+        </div>        <div className='flex gap-4 justify-between p-2'>
+          <img
+            src='https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png'
+            alt=''
+            className='w-20'
+          />
+          <div className='mr-auto flex flex-col justify-between'>
+            <div className='line-clamp-1 font-semibold'>
+              Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops
+            </div>
+            <div>Rs. 88759.11</div>
+            <div className='flex border border-zinc-300 w-fit py-0.5'>
+              <button type='button' className='w-8'>
+                <i className='ri-subtract-fill text-red-600'></i>
+              </button>
+              <div className='text-center'>0</div>
+              <button type='button' className='w-8'>
+                <i className='ri-add-fill text-green-600'></i>
+              </button>
+            </div>
+          </div>
+          <div className='flex flex-col min-w-fit'>
+            <i className='ri-close-large-line ml-auto bg-red-600 text-white p-1.5'></i>
+            <div className='mt-auto ml-auto font-semibold'>Rs. 88759.11</div>
+          </div>
+        </div>
+      </div>
+
+      <div className='mt-auto px-2'>
+        <div className='flex items-center justify-between py-4'>
+          <div className='font-semibold text-lg'>TOTAL: Rs. 00.00</div>
+          <i className='ri-delete-bin-line text-xl bg-red-600 text-white p-1.5'></i>
+        </div>
+        <button
+          type='button'
+          className='bg-black text-white px-4 py-2 w-full font-semibold rounded cursor-pointer flex items-center justify-center gap-1 mt-auto'
+        >
+          <i className='ri-shopping-cart-line text-2xl'></i>
+          <span>Checkout</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
